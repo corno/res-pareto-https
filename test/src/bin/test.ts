1f3e834cea837e87838a3e15df556fa23715edd1
corno <corno.astn.a@schraverus.com>
2022-09-01 21:46:10 +0200

    .

--- test/src/bin/test.ts
+++ test/src/bin/test.ts
@@ -7,12 +7,16 @@ import * as test from "lib-pareto-test"
 import * as diff from "res-pareto-diff"
 import * as fs from "res-pareto-filesystem"
 
-import { getTestSet } from "../imp/getTestSet"
+import { createGetTestSet } from "../imp/createGetTestSet"
+import { dependencies } from "./dependencies"
+
 
 pe.runProgram(
     test.createTester(
         {
-            getTestSet: getTestSet,
+            getTestSet: createGetTestSet(
+                dependencies
+            ),
             diff: {
                 diffData: diff.diffData,
                 stringsAreEqual: diff.stringsAreEqual,
