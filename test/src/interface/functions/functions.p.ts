12471782b3c8f95ac39954449eed0362cd8226e4
corno <corno.astn.a@schraverus.com>
2022-09-25 11:51:29 +0200

    .

--- test/src/interface/functions/functions.p.ts
+++ test/src/interface/functions/functions.p.ts
@@ -2,5 +2,6 @@ import * as test from "lib-pareto-test"
 import { DDependencies } from "../dependencies/dependencies.p"
 
 export type FCreateGetTestset = (
+    $: null,
     $d: DDependencies
-) => test.GetTestSet
+) => test.FGetTestSet
