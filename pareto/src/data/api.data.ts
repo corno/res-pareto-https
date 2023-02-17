5d6a337b8857412e3777b37da6de8a8dd870eb71
corno <corno.astn.a@schraverus.com>
2023-02-17 15:28:32 +0100

    .

--- pareto/src/data/api.data.ts
+++ pareto/src/data/api.data.ts
@@ -8,9 +8,9 @@ import {
     interfaceReference,
     null_,
     method, dictionary, group, member, taggedUnion, types, func, type
-} from "lib-pareto-typescript-project/dist/submodules/glossary/shorthands.p"
+} from "lib-pareto-typescript-project/dist/submodules/glossary/shorthands"
 
-import { definitionReference, constructor, algorithm } from "lib-pareto-typescript-project/dist/submodules/moduleDefinition/shorthands.p"
+import { definitionReference, constructor, algorithm } from "lib-pareto-typescript-project/dist/submodules/moduleDefinition/shorthands"
 
 import * as mmoduleDefinition from "lib-pareto-typescript-project/dist/submodules/moduleDefinition"
 
